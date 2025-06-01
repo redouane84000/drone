9b8bd419a83b34015fcbe7b088d1608df811d5db
redouane <redouanektm@hotmail.fr>
2025-06-01 23:54:10 +0200

    mise a jour mysql

--- config/db.js
+++ config/db.js
@@ -3,20 +3,16 @@ require('dotenv').config();
 const mysql = require('mysql2');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Erreur de connexion à la base de données :', err);
-    } else {
-        console.log('Connexion à la base de données réussie');
-    }
-});
 
 module.exports = { db, stripe };
